67bdef41a5220558facb2d2fd981ac237f9c52dd
SubhanHakim <subhanhakim07@gmail.com>
2026-01-13 21:08:14 +0700

    update x

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -88,7 +88,7 @@ const Hero = () => {
 
                         {/* Social Links */}
                         <div className="flex gap-8 mt-8 items-center pl-6 pointer-events-auto">
-                            <a href="https://x.com/zeusascendant_?s=11" target="_blank" rel="noreferrer" className="flex items-center gap-3 group">
+                            <a href="https://x.com/zeus_ascendant?s=21&t=p8y9jAhkYdmzWlb-DnEN9Q" target="_blank" rel="noreferrer" className="flex items-center gap-3 group">
                                 <div className="w-8 h-8 rounded-full border border-white/10 flex items-center justify-center group-hover:border-electric/50 transition-colors duration-300">
                                     <svg className="w-3 h-3 text-stone-400 group-hover:text-white fill-current transition-colors" viewBox="0 0 24 24" aria-hidden="true">
                                         <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
